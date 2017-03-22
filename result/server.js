45d135a1826fbfa3b31c37c6e938806d9d7c7eff
Michał Jagiełło <michal.jagiello@nask.pl>
2017-03-22 12:29:37 +0100

    Results - reconnect with db

--- result/server.js
+++ result/server.js
@@ -21,29 +21,34 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
-    });
-  },
-  function(err, client) {
-    if (err) {
-      return console.err("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
-  }
-);
+function connect() {
+	async.retry(
+	  {times: 1000, interval: 1000},
+	  function(callback) {
+		pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+		  if (err) {
+			console.error("Waiting for db");
+		  }
+		  callback(err, client);
+		});
+	  },
+	  function(err, client) {
+		if (err) {
+		  return console.err("Giving up");
+		}
+		console.log("Connected to db");
+		getVotes(client);
+	  }
+	);
+}
+connect();
 
 function getVotes(client) {
   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
     if (err) {
       console.error("Error performing query: " + err);
+	  connect();
+	  return;
     } else {
       var votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
